fb17a277ceff2b6f9e7dd9f94e3a38e0451e71e7
Pooja Morye <moryepooja24@gmail.com>
2026-03-13 15:49:36 +0530

    commands practice

--- cypress/e2e/pomLogin.cy.js
+++ cypress/e2e/pomLogin.cy.js
@@ -5,18 +5,18 @@ describe('Login Test', () => {
 const login = new LoginPage()
 
 it('Login with valid credentials', () => {
-cy.fixture('loginData').then((data) => {
+    cy.fixture('loginData').then((data)=>{
+      cy.login(data.username,data.password)
+      cy.contains('You logged into a secure area!')
+      .should('be.visible')
+    })
+  })
 
-login.visit()
-
-login.enterUsername(data.username)
-
-login.enterPassword(data.password)
-
-login.clickLogin()
-
-})
-
-})
-
+  it('Login with invalid credentials', () => {
+    cy.fixture('loginData').then((data)=>{
+      cy.login(data.invalidUser,data.password)
+      cy.contains('Your username is invalid!')
+      .should('be.visible')
+    })
+  })
 })
