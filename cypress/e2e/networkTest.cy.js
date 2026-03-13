38c2c4dfff4fa908db840ec69eebd21c17daae87
Pooja Morye <moryepooja24@gmail.com>
2026-03-13 18:34:51 +0530

    api intercept practice

---
+++ cypress/e2e/networkTest.cy.js
@@ -0,0 +1,17 @@
+describe('Network Intercept Test', () => {
+
+    it('Intercept API call', () => {
+    
+    cy.visit('https://example.cypress.io/commands/network-requests')
+    
+    cy.intercept('GET','**/comments/*').as('getComment')
+    
+    cy.contains('Get Comment').click()
+    cy.wait('@getComment').its('response.statusCode').should('eq',200)
+    
+    cy.contains('Get Comment').click()
+    cy.wait('@getComment').its('response.body').should('have.property','id')
+    
+    })
+    
+    })
